2e99bbb49ed4a6c897c981443e642e378d637fd8
Charles Swaleh <charlswall@live.com>
2023-04-25 14:34:57 +0300

    Added the clickbaity titles to custom validation for the Post class

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,9 +3,13 @@ class Post < ApplicationRecord
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w[Fiction Non-Fiction] }
-  validate :title_is_not_true_facts
+  validate :title_is_not_true_facts_and_more_clickbaity
 
-  def title_is_not_true_facts
-    errors.add(:title, "Title cannot be True Facts!") if title == "True Facts"
+  def title_is_not_true_facts_and_more_clickbaity
+    clickbait_titles = [/Won't Believe/i, /Secret/i, /Top \d/i, /Guess/i]
+
+    if clickbait_titles.none? { |clickbait| clickbait.match title }
+      errors.add(:title, "Your title must be more clickbait-y")
+    end
   end
 end
