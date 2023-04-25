a49b7c192d380e3127e8b55c230d790558643f7b
Charles Swaleh <charlswall@live.com>
2023-04-25 13:59:34 +0300

    Completed validating the Author class

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validate :title_is_not_true_facts
+  validates :content, length: { mininum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category_is_fiction_or_non_fiction
+
+  def title_is_not_true_facts
+    errors.add(:title, "Title cannot be True Facts") if title == "True Facts"
+  end
+
+  def category_is_fiction_or_non_fiction
+    unless category == "Fiction" || category == "Non-Fiction"
+      errors.add(:category, "Category is either Fiction or Non-Fiction")
+    end
+  end
 end
