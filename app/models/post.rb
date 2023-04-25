81829ad4d238f8c8544051d4e6950258b3b3cb04
Charles Swaleh <charlswall@live.com>
2023-04-25 14:09:19 +0300

    Completed the validations and custom validation for the Post class

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,17 +1,11 @@
 class Post < ApplicationRecord
   validates :title, presence: true
-  validate :title_is_not_true_facts
-  validates :content, length: { mininum: 250 }
+  validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
-  validates :category_is_fiction_or_non_fiction
+  validates :category, inclusion: { in: %w[Fiction Non-Fiction] }
+  validate :title_is_not_true_facts
 
   def title_is_not_true_facts
-    errors.add(:title, "Title cannot be True Facts") if title == "True Facts"
-  end
-
-  def category_is_fiction_or_non_fiction
-    unless category == "Fiction" || category == "Non-Fiction"
-      errors.add(:category, "Category is either Fiction or Non-Fiction")
-    end
+    errors.add(:title, "Title cannot be True Facts!") if title == "True Facts"
   end
 end
